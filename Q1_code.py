e0c8153e891eb17cdccfa292aa4e8e0333971595
Daegan Rose-Love <dro92@uclive.ac.nz>
2023-08-02 20:44:55 +1200

    Tidy code, change functions being graphed.

--- Q1_code.py
+++ Q1_code.py
@@ -3,27 +3,37 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    # x values range = [0, 10] with 100 points.
-    x = np.linspace(0, 10, 100)
-    # y value is sin(x)
-    y = np.sin(x)
-    y1 = np.convolve(x, np.sin(x))
+
+    # x values range = [0, 20pi] with 1000 points.
+    x = np.linspace(0, 20*np.pi, 1000)
+
+
+    message = np.sin(x)
+    carrier = np.cos(x)
+
+    result = np.convolve(message, carrier)
     
+    # First subplot: rows, cols, index. Shows message signal. #TODO Currently random placeholder plot.
     plt.subplot(3, 1, 1)
-    plt.title("Before")
-    plt.plot(x, y)
+    plt.title("Message")
+    plt.plot(x, message)
     plt.ylabel("sin(x)")
     plt.grid(True)
 
+    # Second subplot: Shows carrier signal. #TODO Currently random placeholder plot.
     plt.subplot(3, 1, 2)
-    plt.plot(np.arange(0, len(y1), 1), y1)
-    plt.title("After")
-    plt.ylabel("cos(x)")
-    plt.xlabel("x")
+    plt.title("Carrier")
+    plt.plot(x, carrier)
     plt.grid(True)
 
+
+    # Third subplot: Shows resulting signal. #TODO Currently random placeholder plot.
     plt.subplot(3, 1, 3)
-    plt.plot(x, x**2)
+    plt.plot(x, result[:len(x)])
+    plt.title("Result")
+    #plt.ylabel("cos(x)")
+    plt.xlabel("x")
+    plt.grid(True)
 
     plt.show()
 
