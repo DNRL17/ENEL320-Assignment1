3ac24e559b1be887862d425ed8cf988ea1316cd3
Daegan Rose-Love <dro92@uclive.ac.nz>
2023-09-17 16:19:38 +1200

    Change Q1 Signal

--- Q1_code.py
+++ Q1_code.py
@@ -1,60 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 
 
 def main():
 
-    # x values range = [0, 20pi] with 1000 points.
-    x = np.linspace(0, 20*np.pi, 1000)
+    # Generate data
+    t = np.linspace(-10*np.pi, 10*np.pi, 1000)
+    f = signal.square(t, 0.5) * np.sin(t)
+    #f = np.heaviside(t, 0.5)
 
+    # Initialize plots
+    fig, ax = plt.subplots(2, 1)
+    fig.tight_layout(h_pad=2)
 
-    message = np.sin(x)
-    carrier = np.cos(x)
+    # Define figures
+    ax[0].plot(t, f)
+    ax[1].plot(t, np.fft.fft(f))
 
-    result = np.convolve(message, carrier)
-    
-    # First subplot: rows, cols, index. Shows message signal. #TODO Currently random placeholder plot.
-    # plt.subplot(3, 1, 1)
-    # plt.title("Message")
-    # plt.plot(x, message)
-    # plt.ylabel("sin(x)")
-    # plt.grid(True)
+    # Define subplot titles
+    ax[0].set_title('Original Signal')
+    ax[1].set_title('Signal in Fourier Space')
 
-    # Second subplot: Shows carrier signal. #TODO Currently random placeholder plot.
-    # plt.subplot(3, 1, 2)
-    # plt.title("Carrier")
-    # plt.plot(x, carrier)
-    # plt.grid(True)
+    # Add grids
+    ax[0].grid(True)
+    ax[1].grid(True)
 
-
-    # Third subplot: Shows resulting signal. #TODO Currently random placeholder plot.
-    # plt.subplot(3, 1, 3)
-    # plt.plot(x, result[:len(x)])
-    # plt.title("Result")
-    # #plt.ylabel("cos(x)")
-    # plt.xlabel("x")
-    # plt.grid(True)
-
-    # Other plot
-    fc = 5
-    t = np.linspace(0, 1, 1000)
-    b = np.sin(2*np.pi*t*fc)
-    c = np.sin(3*t)
-    d = np.sin(5*t)
-    e = b + c + d
-    n = len(b)
-
-    plt.subplot(3, 1, 1)
-    plt.plot(t, b)
-    plt.grid(True)
-
-    plt.subplot(3, 1, 2)
-    plt.plot(t, np.fft.fft(b))
-    plt.grid(True)
-
-    # plt.subplot(3, 1, 3)
-    # plt.plot(t, np.fft.fft(b))
-    # plt.grid(True)
+    # Name and scale figure
+    fig.suptitle('Modelling a Real-World Signal')
+    plt.subplots_adjust(top=0.85)
 
     plt.show()
 
